2963f6bc81255ffc9035f76a92e344bb22d28b35
sunkz <sunkezheng@outlook.com>
2023-06-09 14:33:20 +0800

    2023-06-09 14:33:20

--- src/main/java/com/sunkz/common/util/FileUtil.java
+++ src/main/java/com/sunkz/common/util/FileUtil.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 
 @Slf4j
 public class FileUtil {
@@ -20,6 +23,19 @@ public class FileUtil {
     @SneakyThrows
     public static BufferedImage captureScreen() {
         return new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+    }
+
+    @SneakyThrows
+    public static void downloadUrl(String url, String path) {
+        InputStream inputStream = new URL(url).openStream();
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, length);
+        }
+        inputStream.close();
+        fileOutputStream.close();
     }
 
 }
