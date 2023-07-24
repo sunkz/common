fc1c2e9c03803f05956423e7a4a691c3e2075bb7
sunkz <sunkezheng@outlook.com>
2023-07-24 14:42:56 +0800

    2023-07-24 14:42:56

--- src/main/java/com/sunkz/common/util/PageUtil.java
+++ src/main/java/com/sunkz/common/util/PageUtil.java
@@ -1,7 +1,8 @@
 package com.sunkz.common.util;
 
-import cn.hutool.core.collection.CollectionUtil;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PageUtil {
@@ -10,12 +11,12 @@ public class PageUtil {
      * pageIndex from 1
      */
     public static <T> List<T> getPage(List<T> source, int pageIndex, int pageSize) {
-        if (CollectionUtil.isEmpty(source) || pageIndex <= 0 || pageSize <= 0) {
-            return null;
+        if (CollectionUtils.isEmpty(source) || pageIndex <= 0 || pageSize <= 0) {
+            return new ArrayList<>(1);
         }
         int start = (pageIndex - 1) * pageSize;
         if (start >= source.size()) {
-            return null;
+            return new ArrayList<>(1);
         }
         int end = (source.size() - start) > pageSize ? start + pageSize : source.size();
         return source.subList(start, end);
