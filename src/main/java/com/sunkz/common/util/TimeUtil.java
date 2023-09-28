ba2f5882f239df436a575ea89ba18ae41fcacaac
sunkz <sun_kz@icloud.com>
2023-09-29 00:16:37 +0800

    2023-09-29 00:16:36

--- src/main/java/com/sunkz/common/util/TimeUtil.java
+++ src/main/java/com/sunkz/common/util/TimeUtil.java
@@ -1,6 +1,7 @@
 package com.sunkz.common.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
@@ -8,6 +9,8 @@ public class TimeUtil {
     public static final String PATTERN_1 = "HH:mm:ss";
     public static final String PATTERN_2 = "mm:ss";
     public static final String PATTERN_3 = "yyyyMMddHHmmss";
+    public static final String PATTERN_4 = "yyyy-MM-dd";
+    public static final String PATTERN_5 = "yyyy-MM-dd HH:mm:ss";
 
     public static String now() {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
