9d09ae8266e34fdfdbf243c7a136972ddf8498a1
sunkezheng <sun_kz@icloud.com>
2024-06-25 11:50:10 +0800

    2024-06-25 11:50:10

--- src/main/java/com/sunkz/common/util/ExcelUtil.java
+++ src/main/java/com/sunkz/common/util/ExcelUtil.java
@@ -1,8 +1,7 @@
 package com.sunkz.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
@@ -10,9 +9,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@Slf4j
 public class ExcelUtil {
-
-    private static final Logger log = LoggerFactory.getLogger(ExcelUtil.class);
 
     public static <T> List<T> parseFromURL(String url, Map<String, String> headerAlias, Class<T> clazz) {
         try {
