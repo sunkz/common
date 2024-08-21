aa25916e0f99f98b4787f26a7a426dd7404ce132
sunkezheng <sun_kz@icloud.com>
2024-08-21 19:52:13 +0800

    2024-08-21 19:52:13

--- src/main/java/com/sunkz/common/util/FileUtil.java
+++ src/main/java/com/sunkz/common/util/FileUtil.java
@@ -5,13 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class FileUtil {
@@ -58,6 +62,19 @@ public class FileUtil {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static List<String> readLines(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
     }
 
 }
